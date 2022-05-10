98ba0036029acc11c8e2d81b28081d6afaaf5e21
Kshitij Garg <51716709+Kshitij-24@users.noreply.github.com>
2022-05-10 07:29:03 +0530

    Added solution - LeetHub

---
+++ 12. Average - GFG/12.-average.cpp
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+// User function Template for C++
+
+double posAverage(vector<int> nums) {
+    // code here
+    double count = 0;
+    double sum = 0;
+    for(int i=0;i<nums.size();i++){
+        if(nums[i]>=0){
+            sum += nums[i];
+            count++;
+        }
+    }
+    double ans = sum/count;
+    return ans;
+}
+
+
+// { Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    string s;
+    getline(cin, s);
+    while (t--) {
+        string str;
+        getline(cin, str);
+        vector<int> nums;
+        istringstream ss(str);
+
+        string num;
+        while (ss >> num) {
+            nums.push_back(stoi(num));
+        }
+
+        double avg = posAverage(nums);
+        avg = ((int)(avg * 100)) / 100.0;
+        printf("%.2f\n", avg);
+    }
+
+    return 0;
+}  // } Driver Code Ends
