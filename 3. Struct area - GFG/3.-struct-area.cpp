bf23f38257ea753a8f12c33b187cbd2d0960d2c9
Kshitij Garg <51716709+Kshitij-24@users.noreply.github.com>
2022-05-18 20:46:15 +0530

    Added solution - LeetHub

---
+++ 3. Struct area - GFG/3.-struct-area.cpp
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct area {
+    int sd;
+    int len, wid;
+};
+
+
+ // } Driver Code Ends
+// User function Template for C++
+
+/*struct area
+{
+    int sd;
+    int len, wid;
+}*/
+void find_area(int side, int le, int wd) {
+    // assign value to side of square
+    // your code here
+    area ar;
+    ar.sd = side;
+    ar.len = le;
+    ar.wid = wd;
+    cout<<ar.sd*ar.sd <<" ";
+    cout<<ar.len*ar.wid ;
+
+    // assign values to length and breadth of rectangle
+    // your code here
+
+    // computing area of square
+    // your code here
+
+    // computing area of rectangle
+    // your code here
+}
+
+// { Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int side, length, wide;
+        cin >> side >> length >> wide;
+
+        find_area(side, length, wide);
+        cout << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
