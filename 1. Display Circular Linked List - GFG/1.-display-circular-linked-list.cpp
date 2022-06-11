d1144f0d6160d5a7ef07bd83a3820a2e564dbe9b
Kshitij Garg <51716709+Kshitij-24@users.noreply.github.com>
2022-06-11 14:04:20 +0530

    Added solution - LeetHub

---
+++ 1. Display Circular Linked List - GFG/1.-display-circular-linked-list.cpp
@@ -0,0 +1,104 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node * next;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+    }
+        
+};
+
+
+
+void makeCircular(Node * head)
+{
+    Node * temp=head;
+    
+    while(temp->next)
+    {
+        temp=temp->next;
+    }
+    temp->next=head;
+}
+
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+/*
+struct Node
+{
+    int data;
+    Node * next;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+    }
+        
+};
+
+*/
+vector<int> displayList(Node *head)
+{
+   //Your code here
+   vector<int> ans;
+   
+   if(head == NULL)
+   return ans;
+   Node *curr = head;
+   while(curr->next != head){
+       ans.push_back(curr->data);
+       curr = curr->next;
+   }
+   ans.push_back(curr->data);
+   return ans;
+}
+
+
+// { Driver Code Starts.
+
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    Node *head=NULL, *tail = NULL;
+	    int x;
+	    cin >> x;
+	    head = new Node(x);
+	    tail = head;
+	    for(int i=0;i<n-1;i++)
+	    {
+	        cin>>x;
+	        tail -> next = new Node(x);
+	        tail = tail -> next;
+	    }
+	    
+	  
+	
+
+	   makeCircular(head);
+	    
+	    vector<int> vec = displayList(head);
+	    for(int i = 0;i<vec.size();i++){
+	        cout<<vec[i]<<" ";
+	    }
+	    
+	    cout<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
