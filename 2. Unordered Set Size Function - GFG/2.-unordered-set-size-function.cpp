b2c585ed9387078e37611047efd5ab94b2c7cc82
Kshitij Garg <51716709+Kshitij-24@users.noreply.github.com>
2022-07-11 12:46:46 +0530

    Added solution - LeetHub

---
+++ 2. Unordered Set Size Function - GFG/2.-unordered-set-size-function.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+// unordered_set::size
+#include <iostream>
+#include <unordered_set>
+#include <string>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+//Complete the function
+int SizeMe(unordered_set<string> myset){
+
+    // Your code here
+   // Use size function
+   return myset.size();
+
+}
+
+
+// { Driver Code Starts.
+
+int main ()
+{
+
+  int t;
+  cin>>t;
+  
+  while(t--){
+
+  string str;
+  cin>>str;
+
+  unordered_set<string> myset;
+
+  myset.insert(str);
+
+  cout<<SizeMe(myset)<<endl;
+
+  }
+  
+  return 0;
+}
+  // } Driver Code Ends
